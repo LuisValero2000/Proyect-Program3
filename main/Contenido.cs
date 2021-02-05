a427ee3ec38324f7ec42444ea17bd3801b6891f7
Luis Valero <67386033+LuisValero2000@users.noreply.github.com>
2021-02-05 15:53:53 -0400

    Add files via upload

--- main/Contenido.cs
+++ main/Contenido.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 namespace ProyectoVSC{
 
@@ -7,14 +8,30 @@ namespace ProyectoVSC{
     }
 
     class Entero: Contenido{
-        private int valor;
+        private Hashtable contenidoCuadro;
 
-        public int getValor() {
-            return this.valor;
+        public Hashtable getContenidoCuadro()
+        {
+            return this.contenidoCuadro;
         }
 
-        public void setValor(int valor) {
-            this.valor = valor;
+        public void setContenidoCuadro(Hashtable contenidoCuadro)
+        {
+            this.contenidoCuadro = contenidoCuadro;
+        }
+
+        public void llenar(RestriccionSudoku c, ConjuntoCuadrado conjunto){
+            Random r = new Random();
+            Hashtable tablaAux = new Hashtable();
+            int aux,i=0;
+            while(i<conjunto.getTamano()){
+                aux = r.Next(9);
+                if (c.condicionContenido(aux)){
+                    tablaAux.Add(i+1,aux);
+                    i++;
+                }   
+            }
+            this.setContenidoCuadro(tablaAux);
         }
     }
 }
